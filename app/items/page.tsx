3c3c01dda5ab47e7cec56accd75250a5546682a1
Armianto Sumitro <asumitro@microsoft.com>
2024-07-22 13:51:04 -0700

    Add add new navigation

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -2,14 +2,26 @@ import { Suspense } from "react";
 import Loading from "./loading";
 import ItemsList from "./itemsList";
 import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Divider from "@mui/material/Divider";
 
 export default function Items() {
     return (
         <>
-            <Typography variant="h3">Items</Typography>
-            <Suspense fallback={<Loading />}>
-                <ItemsList />
-            </Suspense>
+            <Typography variant="h5" sx={{marginBottom: 5}}>Items</Typography>
+            <Paper>
+                <Toolbar>
+                    <Link variant="button" color="inherit" href="/items/add" underline="none">
+                    Add
+                    </Link>
+                </Toolbar>
+                <Divider />
+                <Suspense fallback={<Loading />}>
+                    <ItemsList />
+                </Suspense>
+            </Paper>
         </>
     )
 }

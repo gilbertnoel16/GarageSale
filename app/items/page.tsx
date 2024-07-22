0935ebc0aa7c4627e506745acec48901ef9f34e5
Armianto Sumitro <asumitro@microsoft.com>
2024-07-22 16:42:02 -0700

    Add search input

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -6,18 +6,40 @@ import Toolbar from "@mui/material/Toolbar";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Divider from "@mui/material/Divider";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
+import SearchRounded from "@mui/icons-material/SearchRounded";
+import Add from "@mui/icons-material/Add";
+import IconButton from "@mui/material/IconButton";
 
 export default function Items() {
     return (
         <>
-            <Typography variant="h5" sx={{marginBottom: 5}}>Items</Typography>
-            <Paper>
+            <Typography variant="h5" sx={{ marginBottom: 5 }}>Items</Typography>
+            <Paper sx={{ maxWidth: 'lg' }}>
                 <Toolbar>
+                    <TextField
+                        id="search"
+                        label="Search"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchRounded />
+                                </InputAdornment>
+                            ),
+                        }}
+                        variant="standard"
+                    />
+
                     <Link variant="button" color="inherit" href="/items/add" underline="none">
-                    Add
+                        <IconButton aria-label="delete" disabled color="primary">
+                            <Add />
+                        </IconButton>
                     </Link>
                 </Toolbar>
+
                 <Divider />
+
                 <Suspense fallback={<Loading />}>
                     <ItemsList />
                 </Suspense>

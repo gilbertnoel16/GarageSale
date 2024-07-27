d1bd2ebc9a9116448aba74cd93ff5b966bbcb4c8
Armianto Sumitro <asumitro@microsoft.com>
2024-07-27 09:14:44 -0700

    Restore migrations to previous state

--- prisma/migrations/20240722174857_inventory/migration.sql
+++ prisma/migrations/20240722174857_inventory/migration.sql
@@ -13,19 +13,8 @@ CREATE TABLE "Item" (
     "price" MONEY NOT NULL,
     "donatorId" INTEGER,
     "stock" INTEGER,
-    "transactionId" INTEGER,
 
     CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Transaction" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "totalPrice" MONEY NOT NULL,
-    "paymentMethod" TEXT NOT NULL,
-
-    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -33,6 +22,3 @@ CREATE UNIQUE INDEX "Donator_name_key" ON "Donator"("name");
 
 -- AddForeignKey
 ALTER TABLE "Item" ADD CONSTRAINT "Item_donatorId_fkey" FOREIGN KEY ("donatorId") REFERENCES "Donator"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Item" ADD CONSTRAINT "Item_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "Transaction"("id") ON DELETE SET NULL ON UPDATE CASCADE;

0af3c8ba77b2345349d6886b9f975a42d224b70e
Armianto Sumitro <armiantos@gmail.com>
2024-07-27 07:56:51 -0700

    Fix build error

--- app/checkout/itemsList.tsx
+++ app/checkout/itemsList.tsx
@@ -11,54 +11,76 @@ import Button from "@mui/material/Button";
 import Add from "@mui/icons-material/Add";
 import Remove from "@mui/icons-material/Remove";
 
-export default function ItemsList({ filter, onAddItem, onRemoveItem }) {
-    const [items, setItems] = useState([]);
+type ItemListProps = {
+  filter?: string;
+  onAddItem: any;
+  onRemoveItem: any;
+};
 
-    useEffect(() => {
-        (async () => {
-            const items = await getInStockItems(filter);
-            setItems(items);
-        })();
-    }, [filter]);
+export default function ItemsList({
+  filter,
+  onAddItem,
+  onRemoveItem,
+}: ItemListProps) {
+  const [items, setItems] = useState<
+    {
+      price: number;
+      donator: { name: string } | null;
+      id: string;
+      description: string;
+      donatorId: number | null;
+      stock: number | null;
+    }[]
+  >([]);
 
-    return (
-        <TableContainer>
-            <Table aria-label="Items">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>ID</TableCell>
-                        <TableCell>Description</TableCell>
-                        <TableCell>Price</TableCell>
-                        <TableCell>Stock</TableCell>
-                        <TableCell></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {items?.map((item) => (
-                        <TableRow
-                            key={item.id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {item.id}
-                            </TableCell>
-                            <TableCell>{item.description}</TableCell>
-                            <TableCell>${item.price.toLocaleString()}</TableCell>
-                            <TableCell>{item.stock}</TableCell>
-                            <TableCell>
-                                <ButtonGroup variant="contained" aria-label="Basic button group">
-                                    <Button onClick={() => onRemoveItem(item)}>
-                                        <Remove />
-                                    </Button>
-                                    <Button onClick={() => onAddItem(item)}>
-                                        <Add />
-                                    </Button>
-                                </ButtonGroup>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
+  useEffect(() => {
+    (async () => {
+      const items = await getInStockItems(filter);
+      setItems(items);
+    })();
+  }, [filter]);
+
+  return (
+    <TableContainer>
+      <Table aria-label="Items">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Price</TableCell>
+            <TableCell>Stock</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items?.map((item) => (
+            <TableRow
+              key={item.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {item.id}
+              </TableCell>
+              <TableCell>{item.description}</TableCell>
+              <TableCell>${item.price.toLocaleString()}</TableCell>
+              <TableCell>{item.stock}</TableCell>
+              <TableCell>
+                <ButtonGroup
+                  variant="contained"
+                  aria-label="Basic button group"
+                >
+                  <Button onClick={() => onRemoveItem(item)}>
+                    <Remove />
+                  </Button>
+                  <Button onClick={() => onAddItem(item)}>
+                    <Add />
+                  </Button>
+                </ButtonGroup>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
